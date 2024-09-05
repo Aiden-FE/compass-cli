63d5bb0bfb6a7fb910a2a6534c773b8d17cb5390
Aiden-FE <aiden_jin@outlook.com>
2024-09-05 10:21:44 +0800

    fix: 自更新检查超时时间拉长

--- src/http/npm.ts
+++ src/http/npm.ts
@@ -2,5 +2,10 @@ import Api from './api';
 
 // eslint-disable-next-line import/prefer-default-export
 export function getRepoInfoFromNpm(repoName: string) {
-  return Api.chain().domain('npm').get('/:repoName').path('repoName', repoName).request();
+  return Api.chain()
+    .domain('npm')
+    .config({ timeout: 1000 * 30 })
+    .get('/:repoName')
+    .path('repoName', repoName)
+    .request();
 }
